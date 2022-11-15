007a1976ca9695c694f3c97bd793f346aabc8a89
icibaci2021 <p.icibaci@gmail.com>
2022-11-15 19:38:39 -0300

    alteraçoes do teste automatizado

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -2,8 +2,15 @@
 
 context('funcionalidade login',() =>{
 
-    it('Deve fazer login com sucesso', () =>{
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+    it('Deve fazer login com sucesso', () =>{
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
@@ -14,7 +21,6 @@ context('funcionalidade login',() =>{
     
 
     it('Deve exibir mensagem de erro ao inserir usuário inválido', () =>{
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('c@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
@@ -24,7 +30,6 @@ context('funcionalidade login',() =>{
 
 
     it('Deve exibir mensagem de erro ao inserir senha inválida', () =>{
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('12345678')
         cy.get('.woocommerce-form > .button').click()
---
+++ cypress/integration/produtos.spec.js
@@ -0,0 +1,32 @@
+/// <reference types='cypress' />
+
+describe('Funcionalidade Página de Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('[class="product-block grid"]')
+            //.first() //escolhendo o primeiro item da lista
+            //.last() //escolhendo o último item da lista
+            //.eq(3) //escolher pelo item da lista, lembrando que a contagem começa com 0,1,2
+            .contains('Ariel Roll Sleeve Sweatshirt') //escolher pelo nome do produto
+            .click()
+    });
+
+    it.only('Deve adicionar um produto ao carrinho', () => {
+    var quantidade = 3 
+
+        cy.get('[class="product-block grid"]')
+            .contains('Arcadio Gym Short').click()
+        cy.get('.button-variable-item-36').click()
+        cy.get('.button-variable-item-Blue').click()
+        //cy.get('.plus').click() //botão + na quantidade,então serão adicionados 2 itens
+        cy.get('.input-text').clear().type(quantidade) //selecionando numero da quantidade e inserindo a quantidade desejada para o teste
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Arcadio Gym Short” foram adicionados no seu carrinho.')
+
+    });
+});
